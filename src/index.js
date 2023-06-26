10900474cc417b54195a303a7d4659b68a3faa94
macray <macraykorir254@gmail.com>
2023-06-26 13:54:39 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  let form = document.querySelector('form');
+  let todoText = document.querySelector('#todo-text');
+  let todosList = document.querySelector('#todos-list');
+
+  fetch('http://localhost:3000/todos')
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(todo => {
+        let listItem = document.createElement('li');
+        listItem.textContent = todo.title;
+        todosList.appendChild(listItem);
+      });
+    });
+ 
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let newTodo = todoText.value;
+    let listItem = document.createElement('li');
+    listItem.textContent = newTodo;
+
+    todosList.appendChild(listItem);
+
+    e.target.reset();
+  });
+
 });
